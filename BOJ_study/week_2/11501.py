4688762c6573a88d8e8c7776f5632e6c649fdc81
Min Yong Seo <112611895+tjalsdyd2121@users.noreply.github.com>
2024-10-19 08:51:24 +0000

    BOJ : 11501

---
+++ BOJ_study/week_2/11501.py
@@ -0,0 +1,21 @@
+import sys
+def profit():
+  day = int(sys.stdin.readline())
+  stock = list(reversed(list(map(int,sys.stdin.readline().split()))))
+  max = stock[0]
+  ans, num = 0,0
+  for q in range(day):
+    if  stock[q] >= max:
+      ans += (num * max)
+      max = stock[q]
+      num = 0
+    else:
+      num +=1
+      if q:
+        ans -= stock[q]
+    if q==day-1: 
+      ans +=(num*max)
+  print(ans) 
+T = int(sys.stdin.readline())
+for _ in range(T):
+  profit()
--- BOJ_study/week_2/2012.py
+++ BOJ_study/week_2/2012.py
@@ -1,6 +1,5 @@
 import sys
 N = int(sys.stdin.readline())
 pre = sorted([int(sys.stdin.readline()) for _ in range(N)])
-standard = [(q+1) for q in range(N)]
-ans = [abs(pre[q]-standard[q]) for q in range(N)]
+ans = [abs(pre[q]-(q+1)) for q in range(N)]
 print(sum(ans))
